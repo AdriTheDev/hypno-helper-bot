c6bc25d3932f71ca85c7b10150161e6b29cc6225
AdriTheDev <callumhicks60@gmail.com>
2024-05-01 22:05:30 +0100

    Fixed issue with gifs not being supported in welcome card

--- src/events/guildMemberAdd/welcomeMesage.ts
+++ src/events/guildMemberAdd/welcomeMesage.ts
@@ -5,22 +5,25 @@ import type { CommandKit } from 'commandkit';
 import { Font } from 'canvacord';
 
 export default async function (member: GuildMember, client: Client<true>, handler: CommandKit) {
-  const welcomeData = await welcomeSchema.findOne({ guildId: member.guild.id });
-  if (!welcomeData) return;
+	const welcomeData = await welcomeSchema.findOne({ guildId: member.guild.id });
+	if (!welcomeData) return;
 
-  const channel = member.guild.channels.cache.get(welcomeData.channelId) as GuildTextBasedChannel;
-  if (!channel) return;
+	const channel = member.guild.channels.cache.get(welcomeData.channelId) as GuildTextBasedChannel;
+	if (!channel) return;
 
-  const guild = member.guild;
+	const guild = member.guild;
 
-  Font.loadDefault();
-  await Font.fromFile('src/fonts/Rubik-Bold.ttf');
-  const card = new GreetingsCard()
-    .setAvatar(member.user.displayAvatarURL())
-    .setDisplayName(member.displayName)
-    .setType('welcome')
-    .setMessage(`Thanks for joining! You are member #${guild.memberCount}!`);
+	Font.loadDefault();
+	await Font.fromFile('src/fonts/Rubik-Bold.ttf');
+	const card = new GreetingsCard()
+		.setAvatar(member.user.displayAvatarURL({ forceStatic: true }))
+		.setDisplayName(member.displayName)
+		.setType('welcome')
+		.setMessage(`Thanks for joining! You are member #${guild.memberCount}!`);
 
-  const attachment = new AttachmentBuilder(await card.build({ format: 'png' }), { name: 'welcome.png' });
-  channel.send({ content: welcomeData.message.replace('{user}', member.toString()).replace('{n}', '\n'), files: [attachment] });
+	const attachment = new AttachmentBuilder(await card.build({ format: 'png' }), { name: 'welcome.png' });
+	channel.send({
+		content: welcomeData.message.replace('{user}', member.toString()).replace('{n}', '\n'),
+		files: [attachment],
+	});
 }
